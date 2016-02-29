febac5c869f7af9fa1e5a437e845b81668210d8f
Geert Huls <geert.huls@macadam.eu>
2016-02-29 22:41:36 +0100

    Created initial theme for app

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,11 +2,24 @@ import React from 'react';
 import MessageList from './MessageList.jsx';
 import mui from 'material-ui';
 
+import ThemeManager from 'material-ui/lib/styles/theme-manager';
+import CustomTheme from '../customTheme';
+
 var AppBar = mui.AppBar;
 
 class App extends React.Component {
 	constructor() {
 		super();
+	}
+
+	static childContextTypes = {
+		muiTheme: React.PropTypes.object
+	}
+
+	getChildContext() {
+		return {
+			muiTheme: ThemeManager.getMuiTheme(CustomTheme)
+		}
 	}
 
 	render() {
---
+++ src/customTheme.js
@@ -0,0 +1,12 @@
+import Colors from 'material-ui/lib/styles/colors';
+
+//Check site for different types of color:
+//http://www.material-ui.com/#/customization/colors
+export default {
+	palette: {
+		primary1Color: Colors.blue500,
+		primary2Color: Colors.blue700,
+		primary3Color: Colors.blue100,
+		accent1Color: Colors.pink400
+	}
+};
