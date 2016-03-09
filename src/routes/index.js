7f21f542e9d011a1cc08e2945320d49f068164f9
Geert Huls <geert.huls82@gmail.com>
2016-03-09 23:14:50 +0100

    Disable react router query key

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom'
-import { Router, Route, hashHistory, IndexRoute } from 'react-router'
+import { Router, Route, useRouterHistory, IndexRoute } from 'react-router'
+import { createHashHistory } from 'history';
 
 import App from '../components/App.jsx';
 import Chat from '../components/Chat.jsx';
 import Login from '../components/Login.jsx';
 
+const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
+
 let routes = (
- 	<Router history={hashHistory}>
+ 	<Router history={appHistory}>
  		<Route path="/" component={App}>
  			<IndexRoute component={Chat}/>
 			<Route path="/chat" component={Chat} />
