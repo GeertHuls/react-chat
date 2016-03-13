70e135951aaf66850c919eda09bc5360a0f60451
Geert Huls <geert.huls82@gmail.com>
2016-03-13 18:19:50 +0100

    Require authentication for main chat component

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,14 +6,25 @@ import { createHashHistory } from 'history';
 import App from '../components/App.jsx';
 import Chat from '../components/Chat.jsx';
 import Login from '../components/Login.jsx';
+import ChatStore from '../stores/ChatStore';
 
 const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
+
+function requireAuth(nextState, replace) {
+	var state = ChatStore.getState();
+	if (!state.user) {
+		replace({
+	      pathname: '/login',
+	      state: { nextPathname: nextState.location.pathname }
+	    });
+	}
+}
 
 let routes = (
  	<Router history={appHistory}>
  		<Route path="/" component={App}>
- 			<IndexRoute component={Chat}/>
-			<Route path="/chat" component={Chat} />
+ 			<IndexRoute component={Chat} onEnter={requireAuth}/>
+			<Route path="/chat" component={Chat} onEnter={requireAuth} />
 			<Route path="/login" component={Login} />
 		</Route>
 	</Router>
